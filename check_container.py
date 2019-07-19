7b294d13570840c375d48fbeed7f7bee612b1fa6
AsherHsieh <asherhsiang@wisers.com>
2019-07-19 18:00:55 +0800

    modify the alert mail format and file path

--- check_container.py
+++ check_container.py
@@ -6,8 +6,12 @@ import smtplib
 import socket
 
 # Import the email modules we'll need
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 SendMail = False
+
+sender = "python_check@gmail.com"
+receiver = "username@gmail.com"
 
 # list the name of all running container
 myCmd = 'docker ps --format {{.Names}} > docker_ps_Name.txt'
@@ -19,18 +23,20 @@ lines = NameData.readlines()
 NameData.close()
 
 # open the container_registry
-f = open('container_registry')
+f = open('/home/developer/ServerMgt/container_registry')
 s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
 f.close()
 
 # check CompareDockerNameRecord.txt is existed or not
 cp_dict = {}
-if os.path.exists('CompareDockerNameRecord.txt') == False:
-    newfile = open("CompareDockerNameRecord.txt","w+")
+mail_dict = {}
+if os.path.exists('/tmp/CompareDockerNameRecord.txt') == False:
+    newfile = open("/tmp/CompareDockerNameRecord.txt","w+")
     FirstTime = True
 else:
-    newfile = open("CompareDockerNameRecord.txt","r+")
+    newfile = open("/tmp/CompareDockerNameRecord.txt","r+")
     cp_dict = ast.literal_eval(newfile.read()) # read file as dictionary format
+    mail_dict = cp_dict.copy()
     FirstTime = False
 
 # log remove container 
@@ -46,12 +52,16 @@ for i in range(len(lines)):
             if FirstTime == True: # check if record file is first time created
                 #print(lines[i])
                 cp_dict[lines[i].strip('\n')] = 1
+                mail_dict[lines[i].strip('\n')] = 1
                 SendMail = True # send mail function
             else: # the record file has existed
                 if lines[i].strip('\n') in cp_dict.keys(): #the container name is already in file
                     if int(cp_dict[lines[i].strip('\n')]) < 3:
                         cp_dict[lines[i].strip('\n')] = int(cp_dict[lines[i].strip('\n')]) + 1
+                        mail_dict[lines[i].strip('\n')] = int(mail_dict[lines[i].strip('\n')]) + 1
                         SendMail = True #send mail function
+                        if int(cp_dict[lines[i].strip('\n')]) == 3:
+                            mail_dict[lines[i].strip('\n')] = '<font color="red">3</font>'
                     else:
                         #os.system('docker stop ' + lines[i].strip('\n')) # stop the docker container
                         #os.system('docker rm ' + lines[i].strip('\n'))
@@ -59,23 +69,30 @@ for i in range(len(lines)):
                         log_dict[lines[i].strip('\n')] = 'deleted'
                 else: #the container name first time show up in file
                     cp_dict.update({lines[i].strip('\n'): 1})
+                    mail_dict.update({lines[i].strip('\n'): 1})
                     SendMail = True #send mail function
+
+# remove docker_ps_Name.txt
+os.system('rm -f docker_ps_Name.txt')
 
 # Record the container which has been removed
 if LogRemove:
-    logfile = open("DockerDeleteRecord.txt","w+a")
+    logfile = open("/tmp/DockerDeleteRecord.txt","w+a")
     logfile.write(str(log_dict))
     logfile.close()
 
 if SendMail:
-    msg = MIMEText("There is some container with the wrong format of the name.\nIf you are using these containers, please register the correct name of the container to 'container_registry' file or modify the name of the running container.\nWe will inform this message three times before we stop and remove these containers.\nPlease check on " + socket.gethostname() + " (container_name: counting_times) as below:\n" + str(cp_dict))
+    msg = MIMEMultipart('alternative')
+    fixedtemplate = 'There is some container with the wrong format of the name.<br><font color="red">If you are using these containers, please register the correct name of the container to "container_registry" file or modify the name of the running container.</font><br>We will inform this message <font color="red" size="20">three times</font> before we stop and remove these containers.<br>Please check on <font color="red" size="20">' + socket.gethostname() + '</font> (container_name: counting_times) as below:<br>'
+    content = MIMEText(fixedtemplate + str(mail_dict), 'html')
     # the detail of mail
     msg['Subject'] = '[Important Information] There is some container with the wrong format of the name.'
-    msg['From'] = "python_check@gmail.com"
-    msg['To'] = "username@gmail.com"
+    msg['From'] = sender
+    msg['To'] = receiver
+    msg.attach(content)
     # Send the message via our own SMTP server, but don't include the envelope header.
     send = smtplib.SMTP('smtp.google.com')
-    send.sendmail("python_check@gmail.com", ["username@gmail.com"], msg.as_string())
+    send.sendmail(sender, receiver, msg.as_string())
     send.quit()
 
 #print cp_dict
